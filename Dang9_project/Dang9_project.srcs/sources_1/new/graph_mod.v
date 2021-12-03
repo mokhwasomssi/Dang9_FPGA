b544e6680c64e738ce5c71d179587e5e851dd808
mokhwasomssi <mokhwasomssi@naver.com>
2021-12-03 14:30:34 +0900

    키 입력 추가
    - 4번키를 누르고 있을수록 치는힘(공의 속력)이 커짐
    - 0번 키를 누르면 4번키로 얻은 치는힘을 공에 전가
    - 시간에 따라 공의 속력 감소

--- Dang9_project/Dang9_project.srcs/sources_1/new/graph_mod.v
+++ Dang9_project/Dang9_project.srcs/sources_1/new/graph_mod.v
@@ -26,10 +26,12 @@ parameter CUE_Y_SIZE = 16;
 // ball size
 parameter BALL_SIZE = 12;
 
+// hit force
+parameter MAX_HIT_FORCE = 20;
+
 // 60Hz clock
 wire refr_tick; 
 assign refr_tick = (y==MAX_Y-1 && x==MAX_X-1)? 1 : 0; 
-
 
 // table rgb flag
 wire table_out_on, table_in_on;
@@ -47,41 +49,97 @@ assign ball1_reach_left = (TABLE_IN_L >= (ball1_center_x - BALL_SIZE)) ? 1 : 0;
 assign ball1_reach_right = (TABLE_IN_R <= (ball1_center_x + BALL_SIZE)) ? 1 : 0;
 
 
-// 테이블-공 충돌 시 방향 업데이트
-reg [9:0] ball1_vx_reg;
-reg [9:0] ball1_vy_reg;
-reg [4:0] ball1_vx = 4;
-reg [4:0] ball1_vy = 4;
+// 테이블에 부딪혔을 때 방향 업데이트
+reg signed [1:0] ball1_dir_x;
+reg signed [1:0] ball1_dir_y;
 
 always @(posedge clk or posedge rst) begin
     if(rst) begin
-        ball1_vx_reg <= ball1_vx;
-        ball1_vy_reg <= ball1_vy;
+        ball1_dir_x <= 1;
+        ball1_dir_y <= -1;
     end
     else begin
         if(ball1_reach_top) begin
-            ball1_vy_reg <= ball1_vy;
+            ball1_dir_y <= 1;
         end
         else if (ball1_reach_bottom) begin
-            ball1_vy_reg <= -1*ball1_vy;
+            ball1_dir_y <= -1;
         end
         else if (ball1_reach_left) begin
-            ball1_vx_reg <= ball1_vx;
+            ball1_dir_x <= 1;
         end
         else if (ball1_reach_right) begin 
-            ball1_vx_reg <= -1*ball1_vx;
+            ball1_dir_x <= -1;
         end
     end
 end
 
-// 중심 좌표 업데이트
+// 치는 힘 업데이트
+reg [5:0] hit_force;
+reg [6:0] cnt1;
+
+always @(posedge clk or posedge rst) begin
+   if(rst) begin
+       hit_force <= 0;
+   end
+   else if(refr_tick) begin
+        if(key == 5'h14) // 4번키를 누르고 있으면 치는 힘이 커짐
+            if(hit_force < MAX_HIT_FORCE && cnt1 > 5) begin
+                hit_force <= hit_force + 1;
+                cnt1 <= 0;
+            end
+            else
+                cnt1 <= cnt1 + 1;
+   end
+   else if(key_pulse == 5'h10) // 0번키를 누르면 hit_force 초기화
+        hit_force <= 0;
+end
+
+// 시간에 따라 속력 감소
+reg signed [4:0] ball1_vx;
+reg signed [4:0] ball1_vy;
+reg [6:0] cnt2;
+
+always @(posedge clk or posedge rst) begin
+    if(rst | key_pulse == 5'h10) begin // 0번키를 누르면 치는 힘을 공에 전가해줌. 레지스터 특성상 0으로 초기화되기 전 값이 ball1_vx에 들어감.
+        ball1_vx <= hit_force;
+        ball1_vy <= hit_force;
+    end
+    else if(refr_tick) begin
+        if(cnt2 == 20) begin // 0.3초마다 속력 감소
+            if(ball1_vx > 0) ball1_vx <= ball1_vx - 1;
+            if(ball1_vy > 0) ball1_vy <= ball1_vy - 1;
+            cnt2 <= 0;
+        end
+        else begin
+            cnt2 <= cnt2 + 1;
+        end
+    end
+end
+
+// 최종 속도
+reg signed [9:0] ball1_vx_reg;
+reg signed [9:0] ball1_vy_reg;
+
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        ball1_vx_reg <= 0;
+        ball1_vy_reg <= 0;
+    end
+    else begin
+        ball1_vx_reg <= ball1_dir_x*ball1_vx;
+        ball1_vy_reg <= ball1_dir_y*ball1_vy;
+    end
+end
+
+// 공1 중심 좌표 업데이트
 reg [9:0] ball1_center_x;
 reg [9:0] ball1_center_y;
 
 always @(posedge clk or posedge rst) begin
     if(rst) begin
-        ball1_center_x <= 300;
-        ball1_center_y <= 200;
+        ball1_center_x <= MAX_X/2;
+        ball1_center_y <= MAX_Y/2;
     end
     else if(refr_tick) begin
         ball1_center_x <= ball1_center_x + ball1_vx_reg;
