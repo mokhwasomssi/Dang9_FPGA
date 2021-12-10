b5ff9e21160efbf2acae2a9f61fcecf05cf73f52
jongsang Yoo <30527114+js4ngu@users.noreply.github.com>
2021-12-10 14:53:01 +0900

    공 끼리 반사 로직 절대값 계산 모듈

--- Collision_V_update.v
+++ Collision_V_update.v
@@ -19,364 +19,58 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module Collision_V_update(
+module ballCollision_mod(
     input clk,
     input rst,
-    input [1:0] V1x_NOW,
-    input [1:0] V1y_NOW,
-    input [1:0] V2x_NOW,
-    input [1:0] V2y_NOW,
-    input [1:0] V3x_NOW,
-    input [1:0] V3y_NOW,
 
-    output reg [1:0] V1x_UPDATE,
-    output reg [1:0] V1y_UPDATE,
-    output reg [1:0] V2x_UPDATE,
-    output reg [1:0] V2y_UPDATE,
-    output reg [1:0] V3x_UPDATE,
-    output reg [1:0] V3y_UPDATE
-
-    output reg [1:0] D1x_UPDATE,
-    output reg [1:0] D1y_UPDATE,
-    output reg [1:0] D2x_UPDATE,
-    output reg [1:0] D2y_UPDATE,
-    output reg [1:0] D3x_UPDATE,
-    output reg [1:0] D3y_UPDATE
-    );
-
-    reg ball12_LTRB;
-    reg ball12_RTLB;
-    reg ball12_LBRT;
-    reg ball12_RBLT;
-    reg ball12_LCRC;
-    reg ball12_RCLC;
-    reg ball12_CTCB;
-    reg ball12_CBCT;
-
-    reg ball13_LTRB;
-    reg ball13_RTLB;
-    reg ball13_LBRT;
-    reg ball13_RBLT;
-    reg ball13_LCRC;
-    reg ball13_RCLC;
-    reg ball13_CTCB;
-    reg ball13_CBCT;
-
-    reg ball23_LTRB;
-    reg ball23_RTLB;
-    reg ball23_LBRT;
-    reg ball23_RBLT;
-    reg ball23_LCRC; 
-    reg ball23_RCLC; 
-    reg ball23_CTCB;
-    reg ball23_CBCT;
+    input [9:0] xBall_a;
+    input [9:0] yBall_a;
+    input [9:0] xBall_b;
+    input [9:0] yBall_b;
     
-    always @(posedge clk or posedge rst) begin
-        if(rst) begin
-        end
-        else begin
-            if (ball12_LTRB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if ( ball12_RTLB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_LBRT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_RBLT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end 
-            else if (ball12_LCRC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_RCLC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_CTCB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_CBCT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_LTRB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_RTLB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_LBRT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_RBLT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end 
-            else if (ball13_LCRC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_RCLC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_CTCB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball13_CBCT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end//
-        end  
-    end
+    input [9:0] Vx_NOW_a,
+    input [9:0] Vy_NOW_a,
+    input [9:0] Vx_NOW_b,
+    input [9:0] Vy_NOW_b,
 
-    always @(posedge clk or posedge rst) begin
-        if(rst) begin
-        end
-        else begin
-            if (ball12_LTRB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_RTLB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_LBRT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_RBLT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end 
-            else if (ball12_LCRC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_RCLC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_CTCB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball12_CBCT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball23_LTRB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RTLB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_LBRT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RBLT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end 
-            else if (ball23_LCRC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RCLC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_CTCB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_CBCT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end//
-        end  
-    end
+    output [9:0] Vx_UPDATE_a,
+    output [9:0] Vy_UPDATE_a,
+    output [9:0] Vx_UPDATE_b,
+    output [9:0] Vy_UPDATE_b,
 
-    always @(posedge clk or posedge rst) begin
-        if(rst) begin
-        end
-        else begin
-            if (ball13_LTRB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_RTLB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_LBRT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_RBLT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end 
-            else if (ball13_LCRC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_RCLC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_CTCB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball13_CBCT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V2x_NOW;
-                V1y_UPDATE = V2y_NOW;
-            end
-            else if (ball23_LTRB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RTLB) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_LBRT) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RBLT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= 1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end 
-            else if (ball23_LCRC) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_RCLC) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_CTCB) begin 
-                D1x_UPDATE <= -1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end
-            else if (ball23_CBCT) begin 
-                D1x_UPDATE <= 1;
-                D1y_UPDATE <= -1;
-                V1x_UPDATE = V3x_NOW;
-                V1y_UPDATE = V3y_NOW;
-            end//
-        end  
-    end
+    output [9:0] Dx_UPDATE_a,
+    output [9:0] Dy_UPDATE_a,
+    output [9:0] Dx_UPDATE_b,
+    output [9:0] Dy_UPDATE_b
+    );
+    parameter BALL_SIZE = 30;
+    wire cos, sin;
+    wire VaXp, VaYp;
+    wire VbXp, VbYp;
+    wire VaX,  VaY;
+    wire VbX,  VbY; 
+
+    assign cos = (yBall_b - yBall_a) / BALL_SIZE;
+    assign sin = (xBall_b - xBall_a) / BALL_SIZE;
+    
+    //need to check Human error (eq)
+    assign VaXp = Vx_NOW_b*cos + Vy_NOW_b*sin;
+    assign VaYp = Vy_NOW_a*cos - Vx_NOW_a*sin;
+    assign VbXp = Vx_NOW_a*cos + Vy_NOW_a*sin;
+    assign VbYp = Vy_NOW_b*cos - Vx_NOW_b*sin;
+
+    assign VaX = VaXp*cos - VaYp*sin;
+    assign VaY = VaXp*sin + VaYp*cos;
+    abs_mod abs_inst1 (VaX,Vx_UPDATE_a); //Balla Vx
+    abs_mod abs_inst2 (VaY,Vx_UPDATE_a); //Balla Vy
+    
+    assign VbX = VbXp * cos - VbYp * sin;
+    assign VbY = VbXp * sin + VbYp * cos;
+    abs_mod abs_inst3 (VbX,Vx_UPDATE_b); //Ballb Vx
+    abs_mod abs_inst4 (VbY,Vy_UPDATE_b); //Ballb Vy
+
+    assign Dx_UPDATE_a = (VaX/Vx_UPDATE_a);
+    assign Dy_UPDATE_a = (VaY/Vy_UPDATE_a);
+    assign Dx_UPDATE_b = (VbX/Vx_UPDATE_b);
+    assign Dy_UPDATE_b = (VbY/Vy_UPDATE_b);
 endmodule
