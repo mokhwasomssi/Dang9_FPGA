bd07e78dc87b373b34320ffc97ad1c611fc38752
Jongsang yoo <jamin0513@gmail.com>
2021-12-13 20:18:20 +0900

    공B 속도 조건 추가 필요

--- Dang9_project/Dang9_project.srcs/sources_1/new/defines.v
+++ Dang9_project/Dang9_project.srcs/sources_1/new/defines.v
@@ -10,17 +10,17 @@
 `define MAX_X 640
 `define MAX_Y 480
 
-`define TABLE_OUT_L 20
-`define TABLE_OUT_R 620
-`define TABLE_OUT_T 20
-`define TABLE_OUT_B 460
+`define TABLE_OUT_L 0
+`define TABLE_OUT_R 640
+`define TABLE_OUT_T 0
+`define TABLE_OUT_B 480
 
 `define TABLE_IN_L 40
 `define TABLE_IN_R 600
 `define TABLE_IN_T 40
 `define TABLE_IN_B 440
 
-`define COS(dx) (dx/24) // Ãæµ¹ °¢µµÀÇ cos, sin
+`define COS(dx) (dx/24) // ï¿½æµ¹ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ cos, sin
 `define SIN(dy) (dy/24)
 
 //`define MAX_HIT_FORCE 12
